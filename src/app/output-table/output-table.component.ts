ad9cce2cbd354bf3716de669608774b4b995278c
Christian Herberger <git@kabarakh.de>
2019-11-03 00:11:42 +0100

    TASK: add statistics

--- src/app/output-table/output-table.component.ts
+++ src/app/output-table/output-table.component.ts
@@ -1,3 +1,4 @@
+import { countBy, reduce } from 'lodash';
 import { DiceRollsService } from '../services/dice-rolls.service';
 import { DiceRoll } from '../models/dice-roll';
 import { Component, OnInit, OnDestroy } from '@angular/core';
@@ -12,12 +13,39 @@ import { Subscription } from 'rxjs';
 export class OutputTableComponent implements OnInit, OnDestroy {
   public diceRolls: DiceRoll[];
   protected subject: Subscription;
+  public sumOfAllRolls = 0;
+  public sumOfAllAverages = 0;
+  public displayStatistics = false;
+  public numberOfDiceRolls = {
+    total: 0,
+    betterThanAverage: 0,
+    average: 0,
+    worseThanAverage: 0
+  };
 
   constructor(protected diceRollsService: DiceRollsService) { }
 
   ngOnInit() {
     this.subject = this.diceRollsService.getDiceRolls().subscribe(diceRolls => {
       this.diceRolls = diceRolls;
+      this.numberOfDiceRolls.total = this.diceRolls.length;
+      this.numberOfDiceRolls.average = countBy(this.diceRolls, (diceRoll: DiceRoll) => {
+        return diceRoll.average === diceRoll.sum;
+      }).true || 0;
+      this.numberOfDiceRolls.betterThanAverage = countBy(this.diceRolls, (diceRoll: DiceRoll) => {
+        return diceRoll.average < diceRoll.sum;
+      }).true || 0;
+      this.numberOfDiceRolls.worseThanAverage = countBy(this.diceRolls, (diceRoll: DiceRoll) => {
+        return diceRoll.average > diceRoll.sum;
+      }).true || 0;
+
+      this.sumOfAllRolls = reduce(this.diceRolls, (result: number, diceRoll: DiceRoll) => {
+        return result + diceRoll.sum;
+      }, this.sumOfAllRolls);
+
+      this.sumOfAllAverages = reduce(this.diceRolls, (result: number, diceRoll: DiceRoll) => {
+        return result + diceRoll.average;
+      }, this.sumOfAllAverages);
     });
   }
 
@@ -29,4 +57,11 @@ export class OutputTableComponent implements OnInit, OnDestroy {
     this.diceRollsService.addDiceRoll(input);
   }
 
+
+  displayStatisticsPercentage(input: number = 0): string {
+    if (input === 0) {
+      return '-';
+    }
+    return (input / this.numberOfDiceRolls.total * 100).toFixed(2) + '%';
+  }
 }
